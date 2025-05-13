e4de364a5dc863056e64c4175d9ada38084ea4a4
Robert French <rmfrench704@gmail.com>
2025-05-13 13:35:21 -0400

    move the to-do list out of the main file

---
+++ ToDo_List.md
@@ -0,0 +1,25 @@
+# To-Do List.md
+
+- support for game-specific configs
+
+- text indicating an empty config file
+
+- make the window look prettier
+
+- add a readme
+
+- add a license
+
+- figure out how to use github
+
+- make a pretty icon
+
+- indicate that the old configs are stored in the config file and not deleted
+
+- add more gamescope args
+
+- clicking the apply button should make a popup telling you what the changes are, allowing you to accept or cancel
+
+- add a button to reset the config to default
+
+- add a button to open the config file in a text editor
--- scopebuddy_gui.py
+++ scopebuddy_gui.py
@@ -9,21 +9,6 @@ from ui_about import Ui_Dialog  # Import generated UI file
 
 ## The GUI files were created by: Qt User Interface Compiler version 6.9.0
 
-#TODO:
-'''
-> support for game-specific configs
-> text indicating an empty config file
-> make the window look prettier
-> add a readme
-> add a license
-> figure out how to use github
-> make a pretty icon
-> indicate that the old configs are stored in the config file and not deleted
-> add more gamescope args
-> clicking the apply button should make a popup telling you what the changes are, allowing you to accept or cancel
-> add a button to reset the config to default
-> add a button to open the config file in a text editor
-'''
 
 class MainWindow(QMainWindow):
     def __init__(self):
