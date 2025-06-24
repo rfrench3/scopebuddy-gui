a9972f6872aadedaf45b3e8573fc16503b785269
Robert French <178658682+rfrench3@users.noreply.github.com>
2025-06-24 17:58:46 -0400

    buttons now call on JS functions; JS functions not yet complete

--- web-ui/script.js
+++ web-ui/script.js
@@ -5,3 +5,10 @@ function copyConfigToClipboard() {
         navigator.clipboard.writeText(text);
     }
 }
+
+function generateGamescopeConfig() {
+    const output = document.getElementById('output_config'); // send generated line to this element
+    if (output) {
+        output.textContent = 'test';
+    }
+}
