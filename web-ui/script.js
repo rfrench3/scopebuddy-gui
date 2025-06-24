3ea001631b56847d22f68d0cc8398f783c3fd02f
Robert French <178658682+rfrench3@users.noreply.github.com>
2025-06-24 18:50:50 -0400

    full functionality implemented: only minor UI work is left

--- web-ui/script.js
+++ web-ui/script.js
@@ -31,13 +31,37 @@ function generateGamescopeConfig() {
     let editableList = []; //append each argument to this list
     editableList.push("gamescope");
     // beginning of entries
-
-
+    if (rendered_width.value != 0) {editableList.push(`-w ${rendered_width.value}`)}
+    if (rendered_height.value != 0) {editableList.push(`-h ${rendered_height.value}`)}
+    if (output_width.value != 0) {editableList.push(`-W ${output_width.value}`)}
+    if (output_height.value != 0) {editableList.push(`-H ${output_height.value}`)}
+    if (fps.value != 0) {editableList.push(`-r ${fps.value}`)}
+    if (fullscreen.checked) {editableList.push(`-f`)}
+    if (borderless_window.checked) {editableList.push(`-b`)}
+    if (hdr.checked) {editableList.push(`--hdr-enabled`)}
+    if (steam.checked) {editableList.push(`-e`)}
+    if (mangohud.checked) {editableList.push(`--mangoapp`)}
+    if (force_grab_cursor.checked) {editableList.push(`--force-grab-cursor`)}
+    if (force_internal_fullscreen.checked) {editableList.push(`--force-windows-fullscreen`)}
+    if (mouse_sensitivity.value != 1.00) {editableList.push(`-s ${mouse_sensitivity.value}`)}
+    if (vrr.checked) {editableList.push(`--adaptive-sync`)}
+    if (max_scale_factor.value != 0) {editableList.push(`-m ${max_scale_factor.value}`)}
+    if (upscaler_type.value != 0) {editableList.push(`-S ${upscaler_type.value}`)}//       !!!
+    if (upscaler_filter.value != 0) {editableList.push(`-F ${upscaler_filter.value}`)}//   !!!
+    if (upscaler_sharpness.value != 0) {editableList.push(`--sharpness ${upscaler_sharpness.value}`)}
+    if (additional_args.value != 0) {editableList.push(additional_args.value)}
     // end of entries
     editableList.push("-- %command%");
-    let gamescope_line = editableList.join(' ')
+    let gamescope_line = editableList.join(' ');
     const output = document.getElementById('output_config'); // send generated line to this element
     if (output) {
         output.textContent = gamescope_line;
     }
 }
+
+document.getElementById('generate_gamescope_config').addEventListener('click', function() {
+    if (document.querySelector('form').reportValidity()) {
+        //TODO: make sure width is not set if height is not set
+        generateGamescopeConfig();
+    }
+});
